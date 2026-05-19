113ffcb87d6877899536b9985f7d6d3a281409f1
AMT94-star <aslihan.taskin94@gmail.com>
2026-05-19 10:27:35 +0200

    Initial auth-server setup

---
+++ src/main/java/se/amt/webshopauthgroup7/WebshopAuthGroup7Application.java
@@ -0,0 +1,13 @@
+package se.amt.webshopauthgroup7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopAuthGroup7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopAuthGroup7Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/amt/webshopauthgroup7/WebshopAuthGroup7ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.amt.webshopauthgroup7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopAuthGroup7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

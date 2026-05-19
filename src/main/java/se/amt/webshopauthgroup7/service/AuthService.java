6ac4a1a5429f77286a34bdd795cb7c0989e0fe61
AMT94-star <aslihan.taskin94@gmail.com>
2026-05-19 15:04:02 +0200

    Implemented service and init config

--- src/main/java/se/amt/webshopauthgroup7/service/AuthService.java
+++ src/main/java/se/amt/webshopauthgroup7/service/AuthService.java
@@ -1,4 +1,31 @@
 package se.amt.webshopauthgroup7.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import se.amt.webshopauthgroup7.dto.RegisterRequest;
+import se.amt.webshopauthgroup7.model.Role;
+import se.amt.webshopauthgroup7.model.User;
+import se.amt.webshopauthgroup7.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void registerUser(RegisterRequest registerRequest) {
+        //om user existerar kastas det
+        if (userRepository.existsByUsername(registerRequest.getUsername())) {
+            throw new RuntimeException("User already exists");
+        }
+
+        //nya användare får standardrollen user
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setRole(Role.USER);
+
+        userRepository.save(user);
+    }
 }
